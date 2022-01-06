f165cb002f9c179e485b00c170d57e1b844e4301
yuichi.akutsu <>
2022-01-07 03:04:41 +0900

    sepalate vue files

---
+++ js-vue-app/src/page/top/Top.vue
@@ -0,0 +1,3 @@
+<template src="./top.html"></template>
+<script src="./top.js"></script>
+<style scoped src="./top.css"></style>
---
+++ js-vue-app/src/page/top/top.js
@@ -0,0 +1,8 @@
+export default {
+    name: 'TOP',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
